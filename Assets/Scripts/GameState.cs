128f6031f32231fb66c0240065b898cfe628612a
vexxxiang <prezes1195@gmail.com>
2025-02-12 03:56:11 +0100

    GameState - 40%

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
-
+    public ChessPiece[] AtackingFigure;
     public void CheckState()
     {
         if (!MatState() && !SzachState() && !PatState())
@@ -37,17 +37,33 @@ public class GameState : MonoBehaviour
     {
         var Pieces = ChessGameManager.instance.boardState;
         var isWhiteTurn = ChessGameManager.instance.isWhiteTurn;
-        var AtackingFigure = 0;
+
+        var KingPos = new Vector2Int(0,0);
+        
 
         foreach (ChessPiece i in Pieces)
         {
             if (i.gameObject.CompareTag("King") && i.isWhite == isWhiteTurn)
             {
-                
+                KingPos = i.boardPosition;
+            }
+            else {
+                Debug.Log("Nie znaleizono króla dla aktualnie graj¹cych bierek");
             }
         }
+        foreach (ChessPiece i in Pieces)
+        {
+            bool[,] availableMoves = i.GetAvailableMoves(Pieces);
+            if (availableMoves[KingPos.x, KingPos.y])
+            {
+                
+                AtackingFigure[AtackingFigure.GetLength(0) + 1] = i;
+                Debug.Log("Król atakowany przez: " + i);
+            }
+            
+        }
 
-        return false;
+            return false;
     }
     public virtual bool PatState()
     {
